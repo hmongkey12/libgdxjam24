138e0be6129b825f6487a43015dababf72b796b7
hmongkey12 <khammadaolee@gmail.com>
2023-03-18 16:42:16 -0700

    #2, Adding base of the game project

--- core/src/com/squashjam/game/PixelWars.java
+++ core/src/com/squashjam/game/PixelWars.java
@@ -1,31 +1,20 @@
 package com.squashjam.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.squashjam.game.screens.RenderScreen;
 
-public class PixelWars extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class PixelWars extends Game {
+	public SpriteBatch batch;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new RenderScreen(this));
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/squashjam/game/screens/RenderScreen.java
@@ -0,0 +1,54 @@
+package com.squashjam.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.squashjam.game.PixelWars;
+
+import java.util.*;
+
+public class RenderScreen extends ScreenAdapter {
+    private final PixelWars game;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private Texture backgroundTexture;
+
+    public RenderScreen(PixelWars game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        backgroundTexture = new Texture("background.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+        viewport.apply();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        backgroundTexture.dispose();
+    }
+}
